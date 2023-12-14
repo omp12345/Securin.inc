6a69dcfb75ca8282aded02fc03233ec0a38bf38d
omp12345 <opsgp1999@gmail.com>
2023-12-14 01:23:45 -0800

    ok

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Function to calculate all possible combinations
-function calculateCombinations() {
-  const dieA = [1, 2, 3, 4, 5, 6];
-  const dieB = [1, 2, 3, 4, 5, 6];
+function combination() {
+  const d1 = [1, 2, 3, 4, 5, 6];
+  const d2 = [1, 2, 3, 4, 5, 6];
 
   let combinations = [];
 
-  for (let i = 0; i < dieA.length; i++) {
-    for (let j = 0; j < dieB.length; j++) {
+  for (let i = 0; i < d1.length; i++) {
+    for (let j = 0; j < d2.length; j++) {
       combinations.push({
-        dieA: dieA[i],
-        dieB: dieB[j],
-        sum: dieA[i] + dieB[j],
+        d1: d1[i],
+        d2: d2[j],
+        sum: d1[i] + d2[j],
       });
     }
   }
@@ -20,7 +20,7 @@ function calculateCombinations() {
 
 // Function to display the distribution of combinations
 function displayDistribution(combinations) {
-  let distribution = new Array(11).fill(0); // Array to store distribution for sums 2 to 12
+  let distribution = new Array(11).fill(0);
 
   combinations.forEach((combination) => {
     distribution[combination.sum - 2]++;
@@ -30,7 +30,7 @@ function displayDistribution(combinations) {
 }
 
 // Function to calculate the probability of each sum
-function calculateProbabilities(distribution, totalCombinations) {
+function calculateprobabilities1(distribution, totalCombinations) {
   let probabilities = [];
 
   distribution.forEach((count) => {
@@ -42,27 +42,30 @@ function calculateProbabilities(distribution, totalCombinations) {
 
 // Main function to execute the challenge
 function executeChallenge() {
-  const combinations = calculateCombinations();
+  const combinations = combination();
   const totalCombinations = combinations.length;
   const distribution = displayDistribution(combinations);
-  const probabilities = calculateProbabilities(distribution, totalCombinations);
+  const probabilities = calculateprobabilities1(
+    distribution,
+    totalCombinations
+  );
 
-  // Display results
+  // Display the execution results
   displayResults(combinations, distribution, probabilities);
 }
 
-// Function to display results in the HTML document
+// Function to display results in the HTML document with decrative opporches
 function displayResults(combinations, distribution, probabilities) {
-  const resultContainer = document.getElementById("result-container");
+  const resultContainer = document.getElementById("container");
 
   // Display all possible combinations
-  resultContainer.innerHTML += "<h3>All Possible Combinations:</h3>";
+  resultContainer.innerHTML += "<h3>All Possible_Combinations:</h3>";
   combinations.forEach((combination) => {
-    resultContainer.innerHTML += `<p>Die A: ${combination.dieA}, Die B: ${combination.dieB}, Sum: ${combination.sum}</p>`;
+    resultContainer.innerHTML += `<p>Die A: ${combination.d1}, Die B: ${combination.d2}, Sum: ${combination.sum}</p>`;
   });
 
   // Display distribution
-  resultContainer.innerHTML += "<h3>Distribution of Sums:</h3>";
+  resultContainer.innerHTML += "<h3>Distribution  of Sums:</h3>";
   for (let i = 0; i < distribution.length; i++) {
     resultContainer.innerHTML += `<p>Sum ${i + 2}: ${
       distribution[i]
@@ -80,74 +83,74 @@ function displayResults(combinations, distribution, probabilities) {
 
 executeChallenge();
 
-// >>>>>>>>>>>>>>>>>>>>>>   Part(B) of the question >>>>>>>>>>>>>>>>>>>>>
-
-// Function to undoom the dice
-function undoom_dice(Die_A, Die_B) {
-  // Calculate the original probabilities of each sum
-  const originalProbabilities = calculateProbabilities2(Die_A, Die_B);
-  // console.log(originalProbabilities)
-  // Reattach spots to both dice
-  const New_Die_A = reattachSpots(Die_A, originalProbabilities);
-  const New_Die_B = reattachSpots(Die_B, originalProbabilities);
-
-  // Return the new dice arrays
-  return { New_Die_A, New_Die_B };
+// part b solution
+function generateDieA() {
+  // Create Die A with numbers ≤ 4, including repetitions
+  const dieA = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4];
+  return dieA;
 }
 
-// Function to calculate the probabilities of each sum
-function calculateProbabilities2(Die_A, Die_B) {
-  const probabilities = new Array(11).fill(0); // Initialize an array to store probabilities
+function generateDieB() {
+  // Create Die B with numbers, some > 6
+  const dieB = [2, 4, 6, 8, 10, 12];
+  return dieB;
+}
 
-  // Loop through all combinations and count occurrences of each sum
-  for (const valueA of Die_A) {
-    for (const valueB of Die_B) {
+function isStandardDiceProbability(sum) {
+  // Standard dice probabilities for sums 2 to 12
+  const standardProbabilities = [
+    1 / 36,
+    2 / 36,
+    3 / 36,
+    4 / 36,
+    5 / 36,
+    6 / 36,
+    5 / 36,
+    4 / 36,
+    3 / 36,
+    2 / 36,
+    1 / 36,
+  ];
+
+  // Check if the calculated sum matches standard dice probabilities
+  return (
+    Math.abs(
+      calculateProbabilities(generateDieA(), generateDieB())[sum - 2] -
+        standardProbabilities[sum - 2]
+    ) < 0.0001
+  );
+}
+
+function findValidCombinations() {
+  const dieA = generateDieA();
+  const dieB = generateDieB();
+  const validCombinations = [];
+
+  for (const valueA of dieA) {
+    for (const valueB of dieB) {
+      const sum = valueA + valueB;
+
+      if (isStandardDiceProbability(sum)) {
+        validCombinations.push({ dieA: valueA, dieB: valueB, sum });
+      }
+    }
+  }
+
+  return validCombinations;
+}
+
+// Calculate the probabilities of each sum for a given combination of Die A and Die B
+function calculateProbabilities(dieA, dieB) {
+  let probabilities = new Array(11).fill(0);
+
+  for (const valueA of dieA) {
+    for (const valueB of dieB) {
       probabilities[valueA + valueB - 2]++;
     }
   }
-  console.log(probabilities);
 
-  // Calculate probabilities by dividing the counts by the total number of combinations
-  return probabilities.map((count) => count / (Die_A.length * Die_B.length));
+  return probabilities.map((count) => count / (dieA.length * dieB.length));
 }
 
-// Function to reattach spots to the dice
-function reattachSpots(die, originalProbabilities) {
-  const newDie = [...die]; // Create a copy of the original die array
-
-  // Sort the die faces in descending order of spots
-  newDie.sort((a, b) => b - a);
-
-  // Adjust the spots on each face based on original probabilities
-  for (let i = 0; i < newDie.length; i++) {
-    const maxSpots = Math.min(4, newDie[i]); // Die_A cannot have more than 4 spots on a face
-    const requiredProb = originalProbabilities.reduce(
-      (sum, prob, index) => sum + prob * (index + 2 <= newDie[i]),
-      0
-    );
-
-    // If the required probability is zero, set at least one spot for non-zero probability
-    if (requiredProb === 0) {
-      newDie[i] = 1;
-    } else {
-      // Calculate the new number of spots based on the required probability and constraints
-      newDie[i] = Math.min(
-        maxSpots,
-        Math.ceil(requiredProb * (newDie[i] / maxSpots))
-      );
-    }
-  }
-
-  return newDie;
-}
-
-// Example usage
-const Die_A = [1, 2, 3, 4, 5, 6];
-const Die_B = [1, 2, 3, 4, 5, 6];
-
-const { New_Die_A, New_Die_B } = undoom_dice(Die_A, Die_B);
-
-console.log("Original Die_A:", Die_A);
-console.log("Original Die_B:", Die_B);
-console.log("New Die_A:", New_Die_A);
-console.log("New Die_B:", New_Die_B);
+const validCombinations = findValidCombinations();
+console.log("Valid Combinations:", validCombinations);
