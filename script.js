ba45a24c527ba8136f09efd0f1bd0e1658c7dfb3
Om-Prakash-Mishra <112754710+omp12345@users.noreply.github.com>
2023-12-20 04:46:22 -0800

    Update script.js
    part B is done

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// took a variable for part B for standard probabilities
+
 // Function to calculate all possible combinations
+
 function combination() {
   const d1 = [1, 2, 3, 4, 5, 6];
   const d2 = [1, 2, 3, 4, 5, 6];
@@ -30,11 +33,11 @@ function displayDistribution(combinations) {
 }
 
 // Function to calculate the probability of each sum
-function calculateprobabilities1(distribution, totalCombinations) {
+function calculateprobabilities1(distribution, total) {
   let probabilities = [];
 
   distribution.forEach((count) => {
-    probabilities.push(count / totalCombinations);
+    probabilities.push(count / total);
   });
 
   return probabilities;
@@ -43,18 +46,15 @@ function calculateprobabilities1(distribution, totalCombinations) {
 // Main function to execute the challenge
 function executeChallenge() {
   const combinations = combination();
-  const totalCombinations = combinations.length;
+  const total = combinations.length;
   const distribution = displayDistribution(combinations);
-  const probabilities = calculateprobabilities1(
-    distribution,
-    totalCombinations
-  );
+  const probabilities = calculateprobabilities1(distribution, total);
 
   // Display the execution results
   displayResults(combinations, distribution, probabilities);
 }
 
-// Function to display results in the HTML document with decrative opporches
+// Function to display results in the HTML document with declrative opporches
 function displayResults(combinations, distribution, probabilities) {
   const resultContainer = document.getElementById("container");
 
@@ -83,74 +83,105 @@ function displayResults(combinations, distribution, probabilities) {
 
 executeChallenge();
 
-// part b solution
-function generateDieA() {
-  // Create Die A with numbers ≤ 4, including repetitions
-  const dieA = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4];
-  return dieA;
+// <<<<<<<<part b solution>>>>>>>>
+
+let standard_prob = [
+  1 / 36,
+  2 / 36,
+  3 / 36,
+  4 / 36,
+  5 / 36,
+  6 / 36,
+  5 / 36,
+  4 / 36,
+  3 / 36,
+  2 / 36,
+  1 / 36,
+];
+
+// generate a random number between 1 and 4 (include)
+function newDia_A() {
+  return Math.floor(Math.random() * 4) + 1;
 }
 
-function generateDieB() {
-  // Create Die B with numbers, some > 6
-  const dieB = [2, 4, 6, 8, 10, 12];
-  return dieB;
+//generate a random number between 1 and 6
+function new_dia_B() {
+  return Math.floor(Math.random() * 6) + 1;
 }
 
-function isStandardDiceProbability(sum) {
-  // Standard dice probabilities for sums 2 to 12
-  const standardProbabilities = [
-    1 / 36,
-    2 / 36,
-    3 / 36,
-    4 / 36,
-    5 / 36,
-    6 / 36,
-    5 / 36,
-    4 / 36,
-    3 / 36,
-    2 / 36,
-    1 / 36,
-  ];
-
-  // Check if the calculated sum matches standard dice probabilities
-  return (
-    Math.abs(
-      calculateProbabilities(generateDieA(), generateDieB())[sum - 2] -
-        standardProbabilities[sum - 2]
-    ) < 0.0001
-  );
+//create Die A with random values
+function createdieA() {
+  let die_a = [];
+  for (let i = 0; i < 6; i++) {
+    die_a.push(newDia_A());
+  }
+  return die_a;
 }
 
-function findValidCombinations() {
-  const dieA = generateDieA();
-  const dieB = generateDieB();
-  const validCombinations = [];
+//create Die B with random values
+function createdieB() {
+  let die_b = [];
+  for (let i = 0; i < 6; i++) {
+    die_b.push(new_dia_B());
+  }
+  return die_b;
+}
 
-  for (const valueA of dieA) {
-    for (const valueB of dieB) {
-      const sum = valueA + valueB;
+// Function to calculate probabilities for all possible sums
+function calculateprob(Die_A, Die_B) {
+  const new_prob = new Array(2 * Die_A.length).fill(0);
+  let arr = [];
 
-      if (isStandardDiceProbability(sum)) {
-        validCombinations.push({ dieA: valueA, dieB: valueB, sum });
-      }
+  for (let i = 0; i < Die_A.length; i++) {
+    for (let j = 0; j < Die_B.length; j++) {
+      new_prob[Die_A[i] + Die_B[j] - 2]++;
     }
   }
 
-  return validCombinations;
-}
-
-// Calculate the probabilities of each sum for a given combination of Die A and Die B
-function calculateProbabilities(dieA, dieB) {
-  let probabilities = new Array(11).fill(0);
-
-  for (const valueA of dieA) {
-    for (const valueB of dieB) {
-      probabilities[valueA + valueB - 2]++;
-    }
+  const total = Die_A.length * Die_B.length;
+  for (let k = 0; k < new_prob.length; k++) {
+    new_prob[k] /= total;
+    arr.push((new_prob[k] /= total));
   }
 
-  return probabilities.map((count) => count / (dieA.length * dieB.length));
+  return arr;
 }
 
-const validCombinations = findValidCombinations();
-console.log("Valid Combinations:", validCombinations);
+// Function to compare probabilities with standard probabilities for Part B
+function compareProb(probabilities, standardProbabilities) {
+  for (let i = 0; i < probabilities.length; i++) {
+    const min = 0.0001;
+    if (Math.abs(probabilities[i] - standardProbabilities[i]) >= min) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// new dice sum prob is matching with  standard probabilities
+function findmatch(standProb) {
+  console.log(standProb);
+  let i = 0;
+  while (i < 1000) {
+    let New_Die_A = createdieA();
+    let New_Die_B = createdieB();
+    let probabilities = calculateprob(New_Die_A, New_Die_B);
+    console.log(probabilities);
+    let value = compareProb(probabilities, standProb);
+    console.log(value);
+    if (value) {
+      return { New_Die_A, New_Die_B };
+    } else {
+      console.log("not validate");
+    }
+
+    i++;
+  }
+}
+
+let result = findmatch(standard_prob);
+
+if (result) {
+  console.log("new_dice-A", result.New_Die_A);
+  console.log("new_dice-B:", result.New_Die_B);
+}
